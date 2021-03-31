0fbad4d8e274f0567e914c1f6a8a9bf8fa4696f1
Oleg Matushkin <matushkin.oleg@gmail.com>
2021-03-31 07:49:41 +0300

    Test read xlsx

--- read_from_excel.py
+++ read_from_excel.py
@@ -1,1 +1,8 @@
 import pandas as pd
+
+df = pd.read_excel("Forma_.xlsx", sheet_name="Раздел 1")
+print(df)
+print("\n\n=========================\n\n")
+print(df.info())
+print("\n\n=========================\n\n")
+print(df
